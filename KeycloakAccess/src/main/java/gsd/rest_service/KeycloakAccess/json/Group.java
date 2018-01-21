870664543ef1ddfb00145d9b144870f3c5501f43
DanNemes <dan.nemes@gsdgroup.net>
2018-01-21 13:19:34 +0200

    initial commit

---
+++ KeycloakAccess/src/main/java/gsd/rest_service/KeycloakAccess/json/Group.java
@@ -0,0 +1,68 @@
+package gsd.rest_service.KeycloakAccess.json;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(Include.NON_NULL)
+public class Group {
+
+    @JsonProperty("id")
+    private String id;
+
+    @JsonProperty("name")
+    private String name;
+
+    @JsonProperty("subGroups")
+    private Group subGroups[];
+
+    public Group() {
+	super();
+    }
+
+    public Group(String groupName) {
+	this.name = groupName;
+    }
+
+    public String getId() {
+	return id;
+    }
+
+    public void setId(String id) {
+	this.id = id;
+    }
+
+    public String getName() {
+	return name;
+    }
+
+    public void setName(String name) {
+	this.name = name;
+    }
+
+    @Override
+    public String toString() {
+	StringBuilder builder = new StringBuilder();
+	builder.append("Group [id=");
+	builder.append(id);
+	builder.append(", name=");
+	builder.append(name);
+	builder.append(", subGroups=");
+	builder.append(getChildGroups().toString());
+	builder.append("]");
+	return builder.toString();
+    }
+
+    private StringBuilder getChildGroups() {
+	StringBuilder childGroups = new StringBuilder("");
+	if (subGroups != null && subGroups.length > 0) {
+	    for (Group group : subGroups) {
+		childGroups.append(group.toString());
+	    }
+	}
+	return childGroups;
+    }
+
+}
---
+++ KeycloakAccess/src/main/java/gsd/rest_service/KeycloakAccess/json/UserCredentials.java
@@ -0,0 +1,34 @@
+package gsd.rest_service.KeycloakAccess.json;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class UserCredentials {
+
+    @JsonProperty("type")
+    private String type;
+
+    @JsonProperty("value")
+    private String value;
+
+    public UserCredentials(String type, String value) {
+	this.type = type;
+	this.value = value;
+    }
+
+    public String getType() {
+	return type;
+    }
+
+    public void setType(String type) {
+	this.type = type;
+    }
+
+    public String getValue() {
+	return value;
+    }
+
+    public void setValue(String value) {
+	this.value = value;
+    }
+
+}
