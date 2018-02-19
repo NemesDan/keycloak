e015da1563729072b0645cdd11b13368518ba94f
DanNemes <dan.nemes@gsdgroup.net>
2018-02-19 18:20:26 +0200

    - latest changes

--- KeycloakAccess/src/main/java/gsd/rest_service/json/Client.java
+++ KeycloakAccess/src/main/java/gsd/rest_service/json/Client.java
@@ -9,37 +9,37 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 @JsonInclude(Include.NON_NULL)
 public class Client {
 
-    @JsonProperty("id")
-    private String id;
+	@JsonProperty("id")
+	private String id;
 
-    @JsonProperty("clientId")
-    private String clientId;
+	@JsonProperty("clientId")
+	private String clientId;
 
-    public String getId() {
-	return id;
-    }
+	public String getId() {
+		return id;
+	}
 
-    public void setId(String id) {
-	this.id = id;
-    }
+	public void setId(String id) {
+		this.id = id;
+	}
 
-    public String getClientId() {
-	return clientId;
-    }
+	public String getClientId() {
+		return clientId;
+	}
 
-    public void setClientId(String clientId) {
-	this.clientId = clientId;
-    }
+	public void setClientId(String clientId) {
+		this.clientId = clientId;
+	}
 
-    @Override
-    public String toString() {
-	StringBuilder builder = new StringBuilder();
-	builder.append("Client [id=");
-	builder.append(id);
-	builder.append(", clientId=");
-	builder.append(clientId);
-	builder.append("]");
-	return builder.toString();
-    }
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Client [id=");
+		builder.append(id);
+		builder.append(", clientId=");
+		builder.append(clientId);
+		builder.append("]");
+		return builder.toString();
+	}
 
 }
